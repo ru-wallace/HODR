46e6aa1bb7cd7a7c6d522ba0ddca217281e505b9
Ru Wallace <ru@wallace.scot>
2025-07-08 17:10:21 +0100

    Make sure required packages are installed

--- install.sh
+++ install.sh
@@ -6,6 +6,30 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+#Check if packages are installed
+REQUIRED_PACKAGES=("libglib2.0-dev" "libusb-dev")
+NOT_INSTALLED=()
+for package in "${REQUIRED_PACKAGES[@]}"; do
+    if ! dpkg -l | grep -q "$package"; then
+        echo "Package $package is not installed."
+        NOT_INSTALLED+=("$package")
+    fi
+done
+
+if [ ${#NOT_INSTALLED[@]} -ne 0 ]; then
+    echo "The following required packages are not installed: ${NOT_INSTALLED[*]}"
+    read -p "Do you want to install them with apt? (y/n): " answer
+    if [[ "$answer" != "y" && "$answer" != "Y" ]]; then
+        echo "Exiting installation script."
+        exit 1
+    fi
+    echo "Installing missing packages..."
+    apt update
+    apt install -y "${NOT_INSTALLED[@]}"
+else
+    echo "All required packages are already installed."
+fi
+
 BASE_DIR=$(dirname -- "$( readlink -f -- "$0"; )";)
 
 
@@ -28,7 +52,7 @@ FOUND_DIR=$(find "$MINIFORGE_DIR/pkgs/" -type d -name "andor2-sdk-*" 2>/dev/null
 if [ -n "$FOUND_DIR" ]; then
     ANDOR_DIR="$FOUND_DIR"
     echo "Found Andor2 SDK in: $ANDOR_DIR"
-    sudo -u $SUDO_USER echo "ANDOR_DIR=${ANDOR_DIR}" >> "$BASE_DIR/config.mk"
+    sudo -u $SUDO_USER echo "ANDOR_DIR=${ANDOR_DIR}" > "$BASE_DIR/config.mk"
 else
 
     echo "Andor2 SDK not found. Please ensure it is installed in the Miniforge3 directory."
@@ -40,9 +64,11 @@ else
 fi
 
 
+ln -s "$ANDOR_DIR/lib/libandor.so.2" "/usr/local/lib/libandor.so.2"
+ldconfig
 
 
-UDEV_RULE="KERNEL==\"1-2\", SUBSYSTEM==\"usb\", ATTR{idVendor}==\"136e\", ATTR{idProduct}==\"0001\", GROUP=\"$SUDO_USER\", OWNER=\"$SUDO_USER\""
+UDEV_RULE="SUBSYSTEM==\"usb\", ATTR{idVendor}==\"136e\", ATTR{idProduct}==\"0001\", GROUP=\"$SUDO_USER\", OWNER=\"$SUDO_USER\""
 
 UDEV_RULE_FILE="/etc/udev/rules.d/99-hodr.rules"
 
@@ -56,7 +82,7 @@ echo "UDEV rules reloaded"
 
 echo "Please unplug and replug the device for the changes to take effect."
 
-
+ln -s "./hodr.service" "/etc/systemd/user/hodr.service"
 
 echo "Building DBus headers using dbus-codegen..."
 sudo -u $SUDO_USER make dbus > /dev/null 2>&1
